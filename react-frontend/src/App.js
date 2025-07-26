f7c9d70abb69b453d1623fbddaea694a0e41c374
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-26 06:13:00 +0100

    updated commits by engr ipaye

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Chat from './components/Chat';
 import AdminDashboard from './components/AdminDashboard';
 
 const App = () => {
-  return (
-      <Router>
-        <Switch>
-          <Route path="/chat" component={Chat} />
-          <Route path="/admin" component={AdminDashboard} />
-        </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/chat" />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
